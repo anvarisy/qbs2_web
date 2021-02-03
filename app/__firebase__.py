502d72afc3f7706c547f8cb8876a67b57a74f9ed
Andri nova riswanto <anvarisy@gmail.com>
2021-02-03 15:32:08 +0700

    ok

--- app/__firebase__.py
+++ app/__firebase__.py
@@ -2,9 +2,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import storage
+import os
+from qbs2_web.settings import BASE_DIR
 # Use a service account
 
-cred = credentials.Certificate('config.json')
+loc = os.path.join(BASE_DIR, 'config.json')
+cred = credentials.Certificate(loc)
 firebase_admin.initialize_app(cred,{
      'storageBucket': 'pondok-f2805.appspot.com'
 })
